ccdc29ac36b31661ecc0f300c7fa8f0542c6c201
Galdoba <galdoba@mail.ru>
2021-04-23 19:46:56 +1000

    logviewer

--- pkg/stamp/counters.go
+++ pkg/stamp/counters.go
@@ -1,7 +1,12 @@
 package stamp
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+	"time"
+)
 
+//Seconds - returns string with timestamp in format: [HH:MM:SS]
 func Seconds(seconds int64) string {
 	hh := seconds / 3600
 	mm := (seconds - (hh * 3600)) / 60
@@ -23,4 +28,42 @@ func Seconds(seconds int64) string {
 		sStr = "0" + sStr
 	}
 	return hStr + ":" + mStr + ":" + sStr
+}
+
+//Date - return current date as a string in format: [YYYY-MM-DD]
+func Date() string {
+	y, m, d := time.Now().Date()
+	yy := strconv.Itoa(y)
+	mm := strconv.Itoa(int(m))
+	dd := strconv.Itoa(d)
+	if int(m) < 10 {
+		mm = "0" + mm
+	}
+	if d < 10 {
+		dd = "0" + dd
+	}
+	return yy + "-" + mm + "-" + dd
+}
+
+func IsDate(str string) bool {
+	if len(str) != 10 {
+		return false
+	}
+	parts := strings.Split(str, "-")
+	for i, part := range parts {
+		switch i {
+		case 0:
+			if len(part) != 4 {
+				return false
+			}
+			yy, err := strconv.Atoi(part)
+			if err != nil {
+				return false
+			}
+			if yy < 1000 {
+				return false
+			}
+		}
+	}
+	return true
 }
