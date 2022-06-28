845125325f7c7eee7ae7ebec52a7ffc8044a0f62
Galdoba <galdoba@mail.ru>
2022-06-28 16:42:17 +1000

    logistic decoder

--- pkg/decoder/decoder.go
+++ pkg/decoder/decoder.go
@@ -1,32 +1,4 @@
-package main
-
-import (
-	"fmt"
-	"os"
-
-	"github.com/urfave/cli"
-)
-
-func main() {
-	app := cli.NewApp()
-	app.Version = "v 0.0.0"
-	app.Name = "director"
-	app.Usage = "Scans media streams to tell the story..."
-	app.Flags = []cli.Flag{}
-	app.Commands = []cli.Command{}
-	args := []string{""}
-	if len(args) == 0 {
-		fmt.Println("No arguments provided")
-		os.Exit(0)
-	}
-	if err := app.Run(args); err != nil {
-		fmt.Println("Here is my error:")
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("Here is my story: ...")
-
-}
+package decoder
 
 /*
 fmpeg -i Сквозь_огонь_Through_the_fire.mkv
