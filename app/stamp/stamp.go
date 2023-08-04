d03b9c03bb6b95dd1f21d4cfa2444b54e51f47eb
Galdoba <galdoba@mail.ru>
2023-08-04 15:45:30 +1000

    stamp

--- app/stamp/stamp.go
+++ app/stamp/stamp.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/Galdoba/devtools/cli/command"
+	"github.com/Galdoba/utils"
 )
 
 func main() {
@@ -33,8 +33,18 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
+	lines := utils.LinesFromTXT(folder + `photos.csv`)
 	line := -1
 	for scanner.Scan() {
+		clear, err := command.New(
+			command.CommandLineArguments("clear"),
+			command.Set(command.TERMINAL_ON),
+		)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		clear.Run()
 		line++
 		if line == 0 {
 			continue
@@ -48,21 +58,21 @@ func main() {
 			log.Fatal(e)
 		}
 
-		fmt.Println("\n========================================\n")
+		//fmt.Println("\n========================================\n")
 		found := false
 		scanned := scanner.Text()
 		//		scanned = strings.ReplaceAll(scanned, ":", " ")
 		data := strings.Split(scanned, ",")
 
 		input := folder + data[0]
-		fmt.Println(data)
+		//fmt.Println(data)
 		files, err := IOReadDir(folder)
 		if err != nil {
 			panic(err)
 		}
 		outFolder := ""
 		for _, inList := range files {
-			fmt.Println(inList)
+			//	fmt.Println(inList)
 			if strings.HasSuffix(inList, data[0]) {
 				input = inList
 				found = true
@@ -74,12 +84,13 @@ func main() {
 		}
 		//base := strings.Split(data[0], ".")[0]
 		output := outFolder + "Stamped_" + data[0]
-		fmt.Println("Write line 1:", data[1])
+		//fmt.Println("Write line 1:", data[1])
 		text1.Write([]byte(data[1]))
-		fmt.Println("Write line 2:", data[2])
+		//fmt.Println("Write line 2:", data[2])
 		text2.Write([]byte(data[2]))
 		//text1.WriteString(data[1])
 		//text2.WriteString(data[2])
+		fmt.Printf("Line %v/%v\n", line, len(lines))
 		args = append(args, fmt.Sprintf(`-i %v -vf drawtext=textfile=/home/galdoba/workbench/text1.txt:fontcolor=orange:fontsize=150:x=((w/5)*3):y=((h/100)*85):,drawtext=textfile=/home/galdoba/workbench/text2.txt:fontcolor=orange:fontsize=150:x=((w/5)*3):y=((h/100)*85)+160: -y %v`, input, output))
 		//"drawtext=textfile=text.txt:x=640:y=360:fontsize=24:fontcolor=white"     fontsize=150:x=1002:y=100     5152x3864                             fontsize=h/30: x=(w-text_w)/2: y=(h-text_h*2)
 		command, err := command.New(
@@ -93,7 +104,7 @@ func main() {
 		command.Run()
 		fmt.Println(input)
 		fmt.Println(output)
-		time.Sleep(time.Second)
+		//time.Sleep(time.Microsecond * 1000000)
 		args = []string{}
 		fmt.Println("Delete line 1:", data[1])
 		text1.Truncate(0)
