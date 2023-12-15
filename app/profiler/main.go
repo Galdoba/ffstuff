b1cd90ab913307f390b0cc9620d9e6c3892622e2
Galdoba <galdoba@mail.ru>
2023-12-15 19:28:25 +1000

    profiler Video

--- app/profiler/main.go
+++ app/profiler/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -72,37 +74,32 @@ f = количество замечаний от библиотеки парса
 
 func main() {
 	fmt.Println("Here be profiler")
-	concept()
 }
 
-func concept() {
-	sr := &ScanResult{}
-	sr.Frmt = &Format{Filename: "testName", Nb_streams: 5}
-	data, err := os.ReadFile("/home/galdoba/workbench/flscnr/ffprf/scan.json")
-	//data0, err2 := json.MarshalIndent(&sr, "", "  ")
+func newProfile() *Profile {
+	sr := &Profile{}
+	//	sr.Format = &Format{}
+	return sr
+}
 
-	assertNoError(err)
-	if len(data) == 0 {
-		data, err = json.MarshalIndent(&sr, "", "  ")
-		if err != nil {
-			println(err.Error())
-			//os.Exit(1)
-		}
+func ConsumeJSON(path string) (*Profile, error) {
+	if !strings.HasSuffix(path, ".json") {
+		return nil, fmt.Errorf("file is nit json")
+	}
+	sr := &Profile{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("can't read json: %v", err.Error())
 	}
 	err = json.Unmarshal(data, &sr)
 	if err != nil {
-		errText := fmt.Sprintf("can't unmarshal config data: %v", err.Error())
-		println(errText)
-		os.Exit(1)
+		return nil, fmt.Errorf("can't unmarshal json: %v (%v)", err.Error(), path)
 	}
-	//fmt.Println(string(data))
-	//fmt.Println(string(data0))
-	// if err2 != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	fmt.Println(sr.Frmt.Filename)
-	fmt.Println(sr.Streams[2].Codec_name)
-	fmt.Println(sr.String())
+	err = sr.validate()
+	if err != nil {
+		return sr, fmt.Errorf("validation error: %v", err.Error())
+	}
+	return sr, nil
 }
 
 func assertNoError(err error) {
@@ -111,63 +108,242 @@ func assertNoError(err error) {
 	}
 }
 
-type ScanResult struct {
-	//TestStr `json:"Test"`
-	Frmt    *Format   `json:"format"`
-	Streams []*Stream `json:"streams,omitempty"`
+type Profile struct {
+	Format   *Format   `json:"format"`
+	Streams  []*Stream `json:"streams,omitempty"`
+	warnings []string
+	short    string
+	long     string
 }
 
-func (sr *ScanResult) String() string {
-	s := "file: " + sr.Frmt.Filename + "\n"
-	s += fmt.Sprintf("streams total: %v", len(sr.Streams))
-	return s
+func (p *Profile) validate() error {
+	vSNum := 0
+	aSNum := 0
+	dSNum := 0
+	sSNum := 0
+	warnings := 0
+	for _, stream := range p.Streams {
+		switch stream.Codec_type {
+		default:
+			return fmt.Errorf("unknown codec type: %v", stream.Codec_type)
+		case "video":
+
+			vSize := fmt.Sprintf("%vx%v", stream.Width, stream.Height)
+			switch vSize {
+			case "720x576", "1920x1080", "3840x2160":
+			default:
+				p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> bad video size: [%v]", vSNum, vSize))
+			}
+			fps := stream.R_frame_rate
+			switch fps {
+			case "24/1", "25/1", "24000/1001":
+			case "2997/125": //about valid
+				fps = "24000/1001"
+			default:
+				p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> bad fps: [%v]", vSNum, fps))
+			}
+			sardar := "SAR=" + stream.Sample_aspect_ratio + " DAR=" + stream.Display_aspect_ratio
+			switch sardar {
+			case "SAR=1:1 DAR=16:9", "":
+			case "SAR= DAR=":
+				p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> blank SAR DAR data present", vSNum))
+				warnings++
+			case "SAR=1:1 DAR=1024:429", "SAR=1:1 DAR=37:20", "SAR=1:1 DAR=160:67":
+				//p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> bad (but possible) SAR DAR: [%v]", vSNum, sardar))
+			default:
+				p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> bad SAR DAR: [%v]", vSNum, sardar))
+			}
+			btrate := stream.Bit_rate
+			switch btrate {
+			case "":
+				p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> blank bitRate data present", vSNum))
+			default:
+				btr, err := strconv.Atoi(btrate)
+				if err != nil {
+					panic(err.Error())
+				}
+				if btr == 0 {
+					panic(fmt.Sprintf("stream [0:v:%v] ==> bitRate: [%v]", vSNum, btr))
+				}
+				//p.warnings = append(p.warnings, fmt.Sprintf("stream [0:v:%v] ==> bitRate: [%v]", vSNum, btr))
+			}
+			vSNum++
+		case "audio":
+			aSNum++
+		case "data":
+			dSNum++
+		case "subtitle":
+			sSNum++
+		}
+	}
+	if vSNum > 1 {
+		p.warnings = append(p.warnings, fmt.Sprintf("file ==> %v video streams present", vSNum))
+	}
+	p.short = fmt.Sprintf("%v%v%v%v-%v", ehex(vSNum), ehex(aSNum), ehex(dSNum), ehex(sSNum), ehex(len(p.warnings)))
+	return nil
 }
+
+func ehex(i int) string {
+	switch i {
+	default:
+		return "?"
+	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9:
+		return fmt.Sprintf("%v", i)
+	case 10:
+		return "A"
+	case 11:
+		return "B"
+	case 12:
+		return "C"
+	case 13:
+		return "D"
+	case 14:
+		return "E"
+	case 15:
+		return "F"
+	case 16:
+		return "G"
+	case 17:
+		return "H"
+	case 18:
+		return "J"
+	case 19:
+		return "K"
+	case 20:
+		return "L"
+	case 21:
+		return "M"
+	case 22:
+		return "N"
+	case 23:
+		return "P"
+	case 24:
+		return "Q"
+	case 25:
+		return "R"
+	case 26:
+		return "S"
+	case 27:
+		return "T"
+	case 28:
+		return "U"
+	case 29:
+		return "V"
+	case 30:
+		return "W"
+	case 31:
+		return "X"
+	case 32:
+		return "Y"
+	case 33:
+		return "Z"
+	}
+}
+
+func (p *Profile) Warnings() []string {
+	return p.warnings
+}
+
+func (p *Profile) Short() string {
+	return p.short
+}
+
+// func (sr *Profile) String() string {
+// 	s := "file: " + sr.Format.Filename + "\n"
+// 	s += fmt.Sprintf("streams total: %v", len(sr.Streams))
+// 	return s
+// }
 
 type Format struct {
-	Filename         string            `json:"filename,omitempty"`
-	Nb_streams       int               `json:"nb_streams,omitempty"`
-	Nb_programs      int               `json:"nb_programs,omitempty"`
-	Format_name      string            `json:"format_name,omitempty"`
-	Format_long_name string            `json:"format_long_name,omitempty"`
-	Start_time       string            `json:"start_time,omitempty"`
-	Duration         string            `json:"duration,omitempty"`
-	Size             string            `json:"size,omitempty"`
 	Bit_rate         string            `json:"bit_rate,omitempty"`
+	Duration         string            `json:"duration,omitempty"`
+	Filename         string            `json:"filename,omitempty"`
+	Format_long_name string            `json:"format_long_name,omitempty"`
+	Format_name      string            `json:"format_name,omitempty"`
+	Nb_programs      int               `json:"nb_programs,omitempty"`
+	Nb_streams       int               `json:"nb_streams,omitempty"`
 	Probe_score      int               `json:"probe_score,omitempty"`
+	Size             string            `json:"size,omitempty"`
+	Start_time       string            `json:"start_time,omitempty"`
 	Tags             map[string]string `json:"tags,omitempty"`
 }
 
 type Stream struct {
-	Index               int    `json:"index"`
-	Codec_name          string `json:"codec_name"`
-	Codec_long_name     string `json:"codec_long_name"`
-	Profile             string `json:"profile"`
-	Codec_type          string `json:"codec_type"`
-	Codec_time_base     string `json:"codec_time_base"`
-	Codec_tag_string    string `json:"codec_tag_string"`
-	Codec_tag           string `json:"codec_tag"`
-	Width               int    `json:"width"`
-	Height              int    `json:"height"`
-	Codec_width         int    `json:"coded_width"`
-	Codec_height        int    `json:"coded_height"`
-	Closed_captions     int    `json:"closed_captions"`
-	Has_b_frames        int    `json:"has_b_frames"`
-	SAR                 string `json:"sample_aspect_ratio"`
-	DAR                 string `json:"display_aspect_ratio"`
-	Pix_fmt             string `json:"pix_fmt"`
-	Level               int    `json:"level"`
-	Color_Range         string `json:"color_range"`
-	Color_space         string `json:"color_space"`
-	Field_order         string `json:"field_order"`
-	Refs                int    `json:"refs"`
-	R_frame_rate        string `json:"r_frame_rate"`
-	Avg_frame_rate      string `json:"avg_frame_rate"`
-	Time_base           string `json:"time_base"`
-	Start_pts           int    `json:"start_pts"`
-	Start_time          string `json:"start_time"`
-	Duration_ts         int    `json:"duration_ts"`
-	Duration            string `json:"duration"`
-	Bit_rate            string `json:"bit_rate"`
-	Bits_per_raw_sample string `json:"bits_per_raw_sample"`
-	Nb_frames           string `json:"nb_frames"`
+	Avg_frame_rate       string                  `json:"avg_frame_rate,omitempty"`
+	Bit_rate             string                  `json:"bit_rate,omitempty"`
+	Bits_per_raw_sample  string                  `json:"bits_per_raw_sample,omitempty"`
+	Bits_per_sample      int                     `json:"bits_per_sample,omitempty"`
+	Channel_layout       string                  `json:"channel_layout,omitempty"`
+	Channels             int                     `json:"channels,omitempty"`
+	Chroma_location      string                  `json:"chroma_location,omitempty"`
+	Closed_captions      int                     `json:"closed_captions,omitempty"`
+	Codec_long_name      string                  `json:"codec_long_name,omitempty"`
+	Codec_name           string                  `json:"codec_name,omitempty"`
+	Codec_tag            string                  `json:"codec_tag,omitempty"`
+	Codec_tag_string     string                  `json:"codec_tag_string,omitempty"`
+	Codec_time_base      string                  `json:"codec_time_base,omitempty"`
+	Codec_type           string                  `json:"codec_type,omitempty"`
+	Coded_height         int                     `json:"coded_height,omitempty"`
+	Coded_width          int                     `json:"coded_width,omitempty"`
+	Color_primaries      string                  `json:"color_primaries,omitempty"`
+	Color_range          string                  `json:"color_range,omitempty"`
+	Color_space          string                  `json:"color_space,omitempty"`
+	Color_transfer       string                  `json:"color_transfer,omitempty"`
+	Display_aspect_ratio string                  `json:"display_aspect_ratio,omitempty"`
+	Divx_packed          string                  `json:"divx_packed,omitempty"`
+	Dmix_mode            string                  `json:"dmix_mode,omitempty"`
+	Duration             string                  `json:"duration,omitempty"`
+	Duration_ts          int                     `json:"duration_ts,omitempty"`
+	Field_order          string                  `json:"field_order,omitempty"`
+	Has_b_frames         int                     `json:"has_b_frames,omitempty"`
+	Height               int                     `json:"height,omitempty"`
+	Id                   string                  `json:"id,omitempty"`
+	Index                int                     `json:"index,omitempty"`
+	Is_avc               string                  `json:"is_avc,omitempty"`
+	Level                int                     `json:"level,omitempty"`
+	Loro_cmixlev         string                  `json:"loro_cmixlev,omitempty"`
+	Loro_surmixlev       string                  `json:"loro_surmixlev,omitempty"`
+	Ltrt_cmixlev         string                  `json:"ltrt_cmixlev,omitempty"`
+	Ltrt_surmixlev       string                  `json:"ltrt_surmixlev,omitempty"`
+	Max_bit_rate         string                  `json:"max_bit_rate,omitempty"`
+	Nal_length_size      string                  `json:"nal_length_size,omitempty"`
+	Nb_frames            string                  `json:"nb_frames,omitempty"`
+	Pix_fmt              string                  `json:"pix_fmt,omitempty"`
+	Profile              string                  `json:"profile,omitempty"`
+	Quarter_sample       string                  `json:"quarter_sample,omitempty"`
+	R_frame_rate         string                  `json:"r_frame_rate,omitempty"`
+	Refs                 int                     `json:"refs,omitempty"`
+	Sample_aspect_ratio  string                  `json:"sample_aspect_ratio,omitempty"`
+	Sample_fmt           string                  `json:"sample_fmt,omitempty"`
+	Sample_rate          string                  `json:"sample_rate,omitempty"`
+	Start_pts            int                     `json:"start_pts,omitempty"`
+	Start_time           string                  `json:"start_time,omitempty"`
+	Time_base            string                  `json:"time_base,omitempty"`
+	Width                int                     `json:"width,omitempty"`
+	Side_data_list       []Side_data_list_struct `json:"side_data_list,omitempty"`
+	Tags                 map[string]string       `json:"tags,omitempty"`
+	Disposition          map[string]int          `json:"disposition,omitempty"`
 }
+
+type Side_data_list_struct struct {
+	Side_data map[string]string
+}
+
+/*
+
+
+
+_streams___disposition 20117
+_streams___disposition____attached_pic 20117
+_streams___disposition____clean_effects 20117
+_streams___disposition____comment 20117
+_streams___disposition____default 20117
+_streams___disposition____dub 20117
+_streams___disposition____forced 20117
+_streams___disposition____hearing_impaired 20117
+_streams___disposition____karaoke 20117
+_streams___disposition____lyrics 20117
+_streams___disposition____original 20117
+_streams___disposition____timed_thumbnails 23343
+_streams___disposition____visual_impaired 20117
+*/
---
+++ app/profiler/main_test.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func Test_ConsumeJSON(t *testing.T) {
+	dir := `\\192.168.31.4\buffer\IN\ScanData\input\files\`
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for testNum, e := range entries {
+		path := dir + e.Name()
+		sr, err := ConsumeJSON(path)
+		if err != nil {
+			t.Errorf("%v", err.Error())
+		}
+		warnMsg := ""
+		//fmt.Println(sr.Format.Filename)
+		//fmt.Println(sr.Short())
+		for _, warn := range sr.Warnings() {
+			warnMsg += warn + "\n"
+		}
+		if warnMsg != "" {
+			fmt.Println(testNum, "-------")
+			fmt.Println(sr.Format.Filename)
+			fmt.Println(sr.Short())
+			fmt.Println(warnMsg)
+
+		}
+
+	}
+}
+
+// func TestFields(t *testing.T) {
+// 	dir := `\\192.168.31.4\buffer\IN\ScanData\input\files\`
+// 	entries, err := os.ReadDir(dir)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	//depth1Keys := []string{"_streams", "_format"}
+// 	datamap := make(map[string]int)
+// 	ln := len(entries)
+// 	for eNum, e := range entries {
+// 		// if eNum > 10000 {
+// 		// 	break
+// 		// }
+// 		// //fmt.Println(e.Name())
+// 		// bt, err := os.ReadFile(dir + e.Name())
+// 		// if err != nil {
+// 		// 	t.Errorf("OpenFile: %v", err.Error())
+// 		// }
+// 		// s := string(bt)
+// 		// lines := strings.Split(s, "\n")
+// 		// lastDepth := 0
+// 		// //lastQoutes := ""
+// 		// last1 := ""
+// 		// last2 := ""
+// 		// last3 := ""
+// 		// last4 := ""
+// 		// last5 := ""
+// 		// last6 := ""
+// 		// skip := 0
+// 		// for l, line := range lines {
+
+// 		// 	qt, dp := parseQuoted(line)
+
+// 		// 	if dp <= lastDepth {
+// 		// 		switch dp {
+// 		// 		case 1:
+// 		// 			last2 = ""
+// 		// 			last3 = ""
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 2:
+// 		// 			last2 = ""
+// 		// 			last3 = ""
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 3:
+// 		// 			last3 = ""
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 4:
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 5:
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 6:
+// 		// 			last6 = ""
+
+// 		// 		}
+// 		// 	}
+// 		// 	if strings.Contains(line, " }") {
+// 		// 		skip++
+// 		// 		continue
+// 		// 	}
+// 		// 	skip = 0
+// 		// 	if qt != "" {
+// 		// 		switch dp {
+// 		// 		case 1:
+// 		// 			last1 = qt
+// 		// 			last2 = ""
+// 		// 			last3 = ""
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 2:
+// 		// 			last2 = qt
+// 		// 			last3 = ""
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 3:
+// 		// 			last3 = qt
+// 		// 			last4 = ""
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 4:
+// 		// 			last4 = qt
+// 		// 			last5 = ""
+// 		// 			last6 = ""
+// 		// 		case 5:
+// 		// 			last5 = qt
+// 		// 			last6 = ""
+// 		// 		case 6:
+// 		// 			last6 = qt
+
+// 		// 		}
+// 		// 	}
+
+// 		// 	//fmt.Printf("%v||\n", []byte(line))
+
+// 		// 	//fmt.Println(qt, dp)
+// 		// 	if eNum%84 == 0 {
+// 		// 		fmt.Printf("%v/%v\r", eNum, ln)
+// 		// 	}
+// 		// 	datamap[last1+last2+last3+last4+last5+last6]++
+// 		// 	if last1+last2+last3+last4+last5+last6 == "_format___disposition____timed_thumbnails" {
+// 		// 		fmt.Println(line, l)
+// 		// 		fmt.Println(last1)
+// 		// 		fmt.Println(last2)
+// 		// 		fmt.Println(last3)
+// 		// 		fmt.Println(last4)
+// 		// 		fmt.Println(last5)
+// 		// 		fmt.Println(last6)
+// 		// 		fmt.Println(e.Name())
+
+// 		// 		panic("===================")
+// 		// 	}
+// 		// 	lastDepth = dp
+// 		//}
+// 		sr := &ScanResult{}
+// 		sr.Format = &Format{Filename: "testName", Nb_streams: 5}
+// 		data, err := os.ReadFile(dir + e.Name())
+// 		//data0, err2 := json.MarshalIndent(&sr, "", "  ")
+
+// 		assertNoError(err)
+// 		if len(data) == 0 {
+// 			data, err = json.MarshalIndent(&sr, "", "  ")
+// 			if err != nil {
+// 				println(err.Error())
+// 				//os.Exit(1)
+// 			}
+// 		}
+// 		err = json.Unmarshal(data, &sr)
+// 		if err != nil {
+// 			errText := fmt.Sprintf("can't unmarshal config data: %v", err.Error())
+// 			println(errText)
+// 			os.Exit(1)
+// 		}
+// 		//fmt.Println(string(data))
+// 		//fmt.Println(string(data0))
+// 		// if err2 != nil {
+// 		// 	fmt.Println(err.Error())
+// 		// }
+// 		fmt.Println(sr.Format.Filename)
+// 		//fmt.Println(sr.Streams[2].Codec_name)
+// 		fmt.Println(sr.String(), ln, eNum)
+// 		//time.Sleep(time.Second)
+
+// 	}
+// 	fmt.Println("")
+// 	keys := []string{}
+// 	for k := range datamap {
+// 		keys = append(keys, k)
+// 	}
+// 	sort.Strings(keys)
+// 	for _, k := range keys {
+// 		fmt.Println(k, datamap[k])
+// 	}
+// 	//fmt.Println(datamap)
+
+// }
+
+func parseQuoted(line string) (string, int) {
+	prsed := ""
+	depth := 0
+	for strings.HasPrefix(line, "    ") {
+		prsed += "_"
+		depth++
+		line = strings.TrimPrefix(line, "    ")
+	}
+	if strings.HasPrefix(line, `"`) {
+		parts := strings.Split(line, `"`)
+		prsed += parts[1]
+		return prsed, depth
+	}
+	return "", -1
+}
